a53e65c790993b832d427296226fbc15891e2de9
VISHAN <70902772+Uninterestedguy@users.noreply.github.com>
2023-03-21 00:02:53 +0530

    Add files via upload

---
+++ V1.R
@@ -0,0 +1,18 @@
+#introduction to R
+
+#<!--In this file there will be no code directly--> 
+
+#After downloading the Rstudio please set the working directory by typing setwd("path")
+#or by clicking on more on the files/plot sections
+
+
+
+#you can add comments by "#" symbol for single line
+# for multiple lines you can highlight the script and click
+# "ctrl+shift+c"
+
+#To clear the console you can click "ctrl+l"
+
+#To remove a variable from environment you can use rm() command
+
+#To save a file you can either use save option or save.image() or save(list=ls(all.names=TRUE))
---
+++ V2.R
@@ -0,0 +1,12 @@
+#variables in R
+
+#There are 5 types of variables in R
+
+a=23 #numeric
+b='a' #character
+c=2+3i #complex
+d=2 #integer
+e=TRUE #logical 
+
+#c is used for concatanation of anything and forming a list 
+print(c(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e)))
