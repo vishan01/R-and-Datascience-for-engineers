5f5b9d7d5010fc740338ad20b0bd3026c8511281
VISHAN <70902772+Uninterestedguy@users.noreply.github.com>
2023-03-21 00:14:48 +0530

    Add files via upload

---
+++ Basic R programs/V3.R
@@ -0,0 +1,13 @@
+# BASIC DATA TYPE FUNCTIONS 
+
+#typeof() function
+print(typeof(2))
+
+#is.data_type() function
+a=6
+print(is.double(a))
+
+#as.data_type() function
+b=as.logical(a)
+print(b)
+print(typeof(b))
